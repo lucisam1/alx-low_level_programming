7443016dbc83fa7d69725d3ba3b1fc27dbf56960
lucisam1 <akinlusisamuel@gmail.com>
2023-10-11 15:16:41 -0700

    my task

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,18 @@
-#include <unistd.h>
+#include "holberton.h"
+#include <stdio.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - check the code for Holberton School students.
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Always 0.
  */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+  char c;
+
+  c = 'A';
+  printf("%c: %d\n", c, _isupper(c));
+  c = 'a';
+  printf("%c: %d\n", c, _isupper(c));
+  return (0);
 }
