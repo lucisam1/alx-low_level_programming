07a0ba1f29c4bae66a18244bcca5c4a97e055919
lucisam1 <akinlusisamuel@gmail.com>
2023-10-11 15:24:20 -0700

    my task

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,17 @@
-#include "holberton.h"
-#include <stdio.h>
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-  char c;
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
-  c = 'A';
-  printf("%c: %d\n", c, _isupper(c));
-  c = 'a';
-  printf("%c: %d\n", c, _isupper(c));
-  return (0);
-}
+#endif
